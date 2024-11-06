f5a770ef0b39a2e3ba182c5cfcde94c1b94846dc
Ann <b11130237@gapps.ntust.edu.tw>
2024-11-06 16:00:36 +0000

    feat: get col and row of terminal

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,12 @@ int main() {
 |_| |_|\__,_|\__, |\__,_|___/\___|\__, |\___/
              |___/                |___/)" << std::endl;
 
+    // 2. 取得終端機的行列數，並檢查是否為 nullptr
+    const char* colsEnv = std::getenv("COLUMNS");
+    const char* rowsEnv = std::getenv("LINES");
+
+    int termCols = (colsEnv) ? std::atoi(colsEnv) : 0;
+    int termRows = (rowsEnv) ? std::atoi(rowsEnv) : 0;
+
     return 0;
 }
